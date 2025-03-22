91d4778d0765937da4ed3f64a2e10b338e884aa1
Exomanz <trusch102102@gmail.com>
2025-03-21 19:32:43 -0500

    Changed logic flow to account for sliders

--- ComboSplitter/Services/ComboDataProcessor.cs
+++ ComboSplitter/Services/ComboDataProcessor.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using IPA.Utilities;
+using SiraUtil.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Zenject;
 
 namespace ComboSplitter.Services
 {
     public class ComboDataProcessor : IDisposable
     {
+        [Inject] private readonly SiraLog logger;
         public event Action<ComboSplitterDataPackage> LevelDidFinishWithDataPackageEvent = delegate { };
+
+        public readonly List<BeatmapDataItem> notes = new List<BeatmapDataItem>();
 
         public void LevelDidFinishWithDataPackage(ComboSplitterDataPackage package)
         {
@@ -18,27 +27,55 @@ namespace ComboSplitter.Services
             int[] noteCountArray = new int[2];
             int cuttableLeftNotes = 0;
             int cuttableRightNotes = 0;
-
-            await Task.Run(() =>
+#if DEBUG
+            StringBuilder sb = new StringBuilder();
+            const string f_string = "Idx: {0} | Note Type: {1}/{2} | Beat: {3} \n";
+            notes.Clear();
+#endif
+            await Task.Run(async () =>
             {
                 BeatmapDataItem[] allBeatmapDataItems = beatmapData.allBeatmapDataItems.ToArray();
                 for (int idx = 0; idx < allBeatmapDataItems.Length; idx++)
                 {
                     BeatmapDataItem item = allBeatmapDataItems[idx];
 
-                    if (item.type != BeatmapDataItem.BeatmapDataItemType.BeatmapEvent)
+                    if (item is not BeatmapObjectData objectData)
+                        continue;
+#if DEBUG
+                    notes.Add(item);
+#endif
+                    if (item is NoteData noteData &&
+                        noteData.gameplayType != NoteData.GameplayType.Bomb &&
+                        noteData.time >= startTimeToFilterFrom)
                     {
-                        if (item is NoteData noteData &&
-                            noteData.gameplayType != NoteData.GameplayType.Bomb &&
-                            noteData.time >= startTimeToFilterFrom)
-                        {
-                            if (noteData.colorType == ColorType.ColorA)
-                                cuttableLeftNotes++;
-
-                            else cuttableRightNotes++;
-                        }
+#if DEBUG
+                        sb.Append(string.Format(f_string, notes.IndexOf(item), noteData.gameplayType, noteData.colorType, noteData.beat));
+#endif
+                        _ = noteData.colorType == ColorType.ColorA ?
+                            cuttableLeftNotes++ :
+                            cuttableRightNotes++;
                     }
+
+                    else if (item is SliderData sliderData &&
+                        sliderData.sliceCount > 0 &&
+                        sliderData.time >= startTimeToFilterFrom)
+                    {
+                        int inc = sliderData.sliceCount - 1;
+#if DEBUG
+                        for (int i = 0; i < inc; i++)
+                            sb.Append(string.Format(f_string, notes.IndexOf(item), NoteData.GameplayType.BurstSliderElement, sliderData.colorType, sliderData.beat));
+#endif
+                        _ = sliderData.colorType == ColorType.ColorA ?
+                        cuttableLeftNotes += inc :
+                        cuttableRightNotes += inc;
+                    }
+
+                    else continue;
                 }
+#if DEBUG
+                using StreamWriter sw = new StreamWriter(Path.Combine(UnityGame.UserDataPath, "cs_debug.txt"), false);
+                await sw.WriteAsync(sb.ToString());
+#endif
             });
 
             noteCountArray[0] = cuttableLeftNotes;
